da2d1f38a6d2dddc76377b9cfcbd5e91041ed877
Sl1mj1m14 <keiler104m@gmail.com>
2022-02-25 23:26:43 -0500

    Pick Up Requires E
    Modified the pick up code to now require both collision and pressing E. Added some support for more item types down the line.

--- TacoTopia/Assets/Scripts/ItemCollector.cs
+++ TacoTopia/Assets/Scripts/ItemCollector.cs
@@ -5,12 +5,38 @@ using UnityEngine;
 public class ItemCollector : MonoBehaviour
 {
 
+    private Collider2D collider;
+    
     [SerializeField] private string[] itemTags;
-    private void OnTriggerEnter2D (Collider2D collision) {
+    private int index;
 
-        if (collision.gameObject.CompareTag(itemTags[0]) && Input.GetKeyDown(KeyCode.E)); {
-            Destroy(collision.gameObject);
+    private bool isCollided;
+
+    private void Update() {
+
+        if (isCollided && Input.GetKeyDown(KeyCode.E)) PickUp(index);
+    
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.gameObject.CompareTag(itemTags[0])); {
+            index = 0;
+            isCollided = true;
+            collider = collision;
         }
 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+
+        if (collision.gameObject.CompareTag(itemTags[0])); {
+            index = -1;
+            isCollided = false;
+        }
+
+    }
+
+    private void PickUp(int index) {
+        Destroy(collider.gameObject);
     }
 }
--- TacoTopia/Assets/Scripts/PlayerMovement.cs
+++ TacoTopia/Assets/Scripts/PlayerMovement.cs
@@ -51,7 +51,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     //When touching the ground, set isGrounded to true
-    private bool IsGrounded () {
+    private bool IsGrounded() {
         return Physics2D.BoxCast(collision.bounds.center, collision.bounds.size, 0f, Vector2.down, .1f, ground);
     }
 }
