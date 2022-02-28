cca18122cfca377cf9b96fa60c9ade13df025004
Sl1mj1m14 <keiler104m@gmail.com>
2022-02-27 19:02:05 -0500

    Update Inventory.cs
    Added comments and methods for removing items

--- TacoTopia/Assets/Scripts/Inventory.cs
+++ TacoTopia/Assets/Scripts/Inventory.cs
@@ -9,27 +9,28 @@ public class Inventory : MonoBehaviour
 
     private Item<string>[] slots = new Item<string>[INVENTORY_SIZE];
 
-    // Start is called before the first frame update
     void Start()
     {
+        //Initializing the inventory to be full of "air"
         for (int i=0; i<INVENTORY_SIZE; i++) 
             slots[i] = new Item<string>("Air"); 
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log(slots[0]);
-    }
-
+    /*
+    *   This method returns the item name at the specified index
+    */
     public string GetItem(int index) {
         return slots[index].GetItem();
     }
     
+    /*
+    *   This method adds an item in the first available space, or 
+    *   increases the count of an already existing item
+    */
     public bool AddItem(string item) {
 
-        for (int i=0; i<INVENTORY_SIZE; i++) {
+        for (int i=0; i<slots.Length; i++) {
             if (slots[i].Equals("Air")) {
                 slots[i].Set(item, 1);
                 return true;
@@ -42,9 +43,56 @@ public class Inventory : MonoBehaviour
         return false;
     }
 
+    /*
+    *   This method removes the specified amount of the items at the
+    *   specified index, changing the spot to air if more items are 
+    *   removed than exist
+    */
+    public bool RemoveItems(int position, int amount) {
+        
+        if (position < 0 || position >= slots.Length) return false;
+
+        slots[position].DecreaseAmount(amount);
+
+        if (slots[position].GetAmount() <= 0) slots[position].Set("Air", 0);
+
+        return true;
+    }
+
+    /*
+    *   This method removes the specified amount of the specified item, 
+    *   changing the spot to air if more items are removed than exist
+    */
+    public bool RemoveItems(string name, int amount) {
+
+        for (int i=0; i<slots.Length; i++) {
+            if (slots[i].Equals(name)) return RemoveItems(i, amount);
+        }
+
+        return false;
+    }
+
+    /*
+    *   This method removes one of the item at the 
+    *   specified position
+    */
+    public bool RemoveItem(int position) {
+        return RemoveItems(position, 1);
+    }
+
+    /*
+    *   This method removes one of the specified item
+    */
+    public bool RemoveItem(string name) {
+        return RemoveItems(name, 1);
+    }
+
+    /*
+    *   This method switchs the positions of two items
+    */
     public bool SwitchItems(int num1, int num2) {
 
-        if (num1 < 0 || num1 > slots.Length || num2 < 0 || num2 > slots.Length) return false;
+        if (num1 < 0 || num1 >= slots.Length || num2 < 0 || num2 >= slots.Length) return false;
 
         Item<string> temp = new Item<string>(slots[num1].GetItem(),slots[num1].GetAmount());
 
@@ -56,6 +104,9 @@ public class Inventory : MonoBehaviour
 
     }
 
+    /*
+    *   This method returns the amount of empty slots
+    */
     public int GetEmptySlots() {
 
         int emptySlots = 0; 
